cf2a30a6d8291b9232e1a8967c989bad8425ae88
Bruno Nascimento <bruno.bmn@gmail.com>
2022-04-12 16:23:51 -0300

    Initial Commit

---
+++ fakeBank/fakeBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  fakeBankApp.swift
+//  fakeBank
+//
+//  Created by Bruno Nascimento on 12/04/22.
+//
+
+import SwiftUI
+
+@main
+struct fakeBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
